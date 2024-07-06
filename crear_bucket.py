d85c17b5f889fc35f5cb37cfa34d04008f867767
Diego Randolp <132240895+diegorandolp@users.noreply.github.com>
2024-07-06 08:39:22 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -5,9 +5,7 @@ def lambda_handler(event, context):
 	nombre_bucket = event['body']['bucket']
     # Proceso
     s3 = boto3.client('s3')
-    response = client.create_bucket(
-    	Bucket=nombre_bucket
-	)
+    response = client.create_bucket(Bucket=nombre_bucket)
     # Salida
     return {
         'statusCode': 200,
